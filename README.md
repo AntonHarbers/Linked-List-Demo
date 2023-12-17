d0e67c6007222f201d67d2c3b49abeff643a8763
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2023-12-17 22:23:50 +0400

    Refactored Code, added some test code, added Documentation to README

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# odin-linked-lists
+# Linked List - The Odin Project
+
+A linked list implementation using javascript for the odin project.
+
+To run this code just clone the repo and run the following code in your terminal:
+
+CMD:
+
+```
+    node linkedList
+```
+
+Important: You need to have node JS installed on your system.
+
+## Folder Structure
+
+```
+    /.git           -> This git repository
+    linkedList.js   -> Implementation of a linked list class
+    README.md       -> This readme file
+```
+
+## Key Concepts
+
+### Classes
+
+Classes are a blueprint for creating objects in JavaScript, encapsulating data and functions that operate on that data. Introduced in ES6, they provide a more intuitive and structured way to create object-oriented code.
+
+Example:
+Here's how a class is defined and used in your LinkedList implementation:
+
+JS:
+
+```
+    class Node {
+        constructor(value = null) {
+            this.value = value;
+            this.pointer = null;
+        }
+    }
+
+    class LinkedList {
+        constructor() {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+
+        // Methods like append, prepend, etc.
+    }
+```
+
+Notes:
+Node and LinkedList are classes. Node represents a single element in the list, while LinkedList manages the overall list structure.
+The constructor method is used for initializing new objects.
+
+### Linked List Data Structure
+
+A linked list is a linear data structure where each element (node) contains a reference (pointer) to the next node in the sequence. This structure allows for efficient insertion and removal of elements from any position in the list.
+
+Example:
+The append method adds a new node to the end of the list:
+
+JS:
+
+```
+    append(value) {
+        let node = new Node(value);
+
+        if (this.size === 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.pointer = node;
+            this.tail = node;
+        }
+
+        this.size++;
+    }
+```
+
+Notes:
+This method handles both an empty list and a non-empty list, updating head, tail, and size appropriately.
+Node instances are used to store data and the reference to the next node.
+
+## Final Notes
+
+The implementation of a linked list using classes in JavaScript demonstrates the object-oriented approach and its efficiency in managing dynamic data. The linked list's ability to insert and remove elements without reorganizing the entire data structure makes it a versatile and useful choice in various applications.
--- linkedList.js
+++ linkedList.js
@@ -1,208 +1,249 @@
 class Node {
-    constructor() {
-        this.value = null;
-        this.pointer = null;
-    }
+  constructor(value = null) {
+    this.value = value;
+    this.pointer = null;
+  }
 }
 
-class LinkedList{
-    constructor() {
-        this.head = null;
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  append(value) {
+    let node = new Node(value);
+
+    if (this.size === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.pointer = node;
+      this.tail = node;
+    }
+
+    this.size++;
+  }
+
+  prepend(value) {
+    let node = new Node(value);
+
+    if (this.size === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.pointer = this.head;
+      this.head = node;
+    }
+
+    this.size++;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  pop() {
+    if (this.size === 0) {
+      return null;
+    } else if (this.size === 1) {
+      let node = this.head;
+      this.head = null;
+      this.tail = null;
+      this.size--;
+      return node.value;
+    } else {
+      let node = this.tail;
+      let currentNode = this.head;
+
+      while (currentNode.pointer !== this.tail) {
+        currentNode = currentNode.pointer;
+      }
+
+      currentNode.pointer = null;
+      this.tail = currentNode;
+      this.size--;
+      return node.value;
+    }
+  }
+
+  at(index) {
+    if (index >= this.size) {
+      return null;
+    } else {
+      let node = this.head;
+      for (let i = 0; i < index; i++) {
+        node = node.pointer;
+      }
+      return node.value;
+    }
+  }
+
+  contains(value) {
+    let node = this.head;
+
+    while (node) {
+      if (node.value === value) {
+        return true;
+      }
+
+      node = node.pointer;
+    }
+
+    return false;
+  }
+
+  find(value) {
+    let node = this.head;
+
+    while (node) {
+      if (node.value === value) {
+        return node;
+      }
+
+      node = node.pointer;
+    }
+
+    return null;
+  }
+
+  toString() {
+    let node = this.head;
+    let str = '';
+
+    while (node) {
+      str += node.value + ' ';
+      node = node.pointer;
+    }
+
+    return str.trim();
+  }
+
+  insertAt(value, index) {
+    if (index > this.size) {
+      return null;
+    }
+
+    let node = new Node(value);
+
+    if (index === 0) {
+      node.pointer = this.head;
+      this.head = node;
+      if (this.size === 0) {
+        this.tail = node;
+      }
+    } else {
+      let currentNode = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.pointer;
+      }
+      node.pointer = currentNode.pointer;
+      currentNode.pointer = node;
+      if (index === this.size) {
+        this.tail = node;
+      }
+    }
+
+    this.size++;
+  }
+
+  removeAt(index) {
+    if (index >= this.size) {
+      return null;
+    }
+
+    if (index === 0) {
+      let node = this.head;
+      this.head = node.pointer;
+      if (this.size === 1) {
         this.tail = null;
-        this.size = 0;
+      }
+      this.size--;
+      return node.value;
+    } else {
+      let currentNode = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.pointer;
+      }
+      let node = currentNode.pointer;
+      currentNode.pointer = node.pointer;
+      if (index === this.size - 1) {
+        this.tail = currentNode;
+      }
+      this.size--;
+      return node.value;
     }
+  }
 
-    head(){
-        return this.head;
+  printList() {
+    let node = this.head;
+    let str = '';
+
+    while (node) {
+      str += node.value + ' ';
+      node = node.pointer;
     }
-
-    tail(){
-        return this.tail;
-    }
-
-    size(){
-        return this.size;
-    }
-
-    append(value) {
-        let node = new Node();
-        node.value = value;
-
-        if (this.size === 0) {
-            this.head = node;
-            this.tail = node;
-        } else {
-            this.tail.pointer = node;
-            this.tail = node;
-        }
-
-        this.size++;
-    }
-
-    prepend(value) {
-        let node = new Node();
-        node.value = value;
-
-        if (this.size === 0) {
-            this.head = node;
-            this.tail = node;
-        } else {
-            node.pointer = this.head;
-            this.head = node;
-        }
-
-        this.size++;
-    }
-
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    pop(){
-        if (this.size === 0) {
-            return null;
-        } else if (this.size === 1) {
-            let node = this.head;
-            this.head = null;
-            this.tail = null;
-            this.size--;
-            return node.value;
-        } else {
-            let node = this.tail;
-            let currentNode = this.head;
-
-            while (currentNode.pointer !== this.tail) {
-                currentNode = currentNode.pointer;
-            }
-
-            currentNode.pointer = null;
-            this.tail = currentNode;
-            this.size--;
-            return node.value;
-        }
-    }
-
-    at(index){
-        if (index >= this.size) {
-            return null;
-        } else {
-            let node = this.head;
-            let i = 0;
-
-            while (i < index) {
-                node = node.pointer;
-                i++;
-            }
-
-            return node.value;
-        }
-    }
-
-    contains(value){
-        let node = this.head;
-
-        while (node) {
-            if (node.value === value) {
-                return true;
-            }
-
-            node = node.pointer;
-        }
-
-        return false;
-
-    }
-
-    find(value){
-        let node = this.head;
-
-        while (node) {
-            if (node.value === value) {
-                return node;
-            }
-
-            node = node.pointer;
-        }
-
-        return null;
-    }
-
-    toString(){
-        let node = this.head;
-        let str = '';
-
-        while (node) {
-            str += node.value + ' ';
-            node = node.pointer;
-        }
-
-        return str;
-    }
-
-    insertAt(value, index){
-
-        if (index >= this.size) {
-            return null;
-        } else {
-            let node = new Node();
-            node.value = value;
-
-            if (index === 0) {
-                node.pointer = this.head;
-                this.head = node;
-            } else {
-                let currentNode = this.head;
-                let i = 0;
-
-                while (i < index - 1) {
-                    currentNode = currentNode.pointer;
-                    i++;
-                }
-
-                node.pointer = currentNode.pointer;
-                currentNode.pointer = node;
-            }
-
-            this.size++;
-        }
-    }
-
-    removeAt(index){
-        if (index >= this.size) {
-            return null;
-        } else {
-            let node = this.head;
-
-            if (index === 0) {
-                this.head = node.pointer;
-            } else {
-                let currentNode = this.head;
-                let i = 0;
-                
-                while (i < index - 1) {
-                    
-                    currentNode = currentNode.pointer;
-                    i++;
-                }
-
-                node = currentNode.pointer;
-                currentNode.pointer = node.pointer;
-            }
-
-            this.size--;
-            return node.value;
-        }
-    }
-
-    printList(){
-        let node = this.head;
-        let str = '';
-
-        while (node) {
-            str += node.value + ' ';
-            node = node.pointer;
-        }
-
-        console.log(str);
-    }
+    console.log(str.trim());
+  }
 }
+
+// Instantiate the LinkedList
+let myList = new LinkedList();
+
+// Append elements to the list
+myList.append(10);
+myList.append(20);
+myList.append(30);
+
+// Print the list to see the appended elements
+console.log('After appending 10, 20, 30:');
+myList.printList(); // Expected output: 10 20 30
+
+// Prepend an element
+myList.prepend(5);
+
+// Print the list to see the list after prepending
+console.log('After prepending 5:');
+myList.printList(); // Expected output: 5 10 20 30
+
+// Check the size of the list
+console.log('Size of the list:', myList.getSize()); // Expected output: 4
+
+// Check if the list contains a value
+console.log('Does the list contain 20?', myList.contains(20)); // Expected output: true
+console.log('Does the list contain 99?', myList.contains(99)); // Expected output: false
+
+// Remove the last element and print the list
+console.log('Value popped:', myList.pop()); // Expected output: 30
+console.log('After popping:');
+myList.printList(); // Expected output: 5 10 20
+
+// Insert at a specific index and print the list
+myList.insertAt(15, 1);
+console.log('After inserting 15 at index 1:');
+myList.printList(); // Expected output: 5 15 10 20
+
+// Remove at a specific index and print the list
+console.log('Value removed at index 2:', myList.removeAt(2)); // Expected output: 10
+console.log('After removing value at index 2:');
+myList.printList(); // Expected output: 5 15 20
+
+// Get value at a specific index
+console.log('Value at index 1:', myList.at(1)); // Expected output: 15
+
+// Print the final state of the list
+console.log('Final state of the list:');
+myList.printList(); // Expected output: 5 15 20
